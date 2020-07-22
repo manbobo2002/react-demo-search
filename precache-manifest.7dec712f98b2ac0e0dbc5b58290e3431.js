050ecf23021d52251598154a52690559254710fd
MPLeung <manbobo2012@gmail.com>
2020-07-22 05:24:56 +0000

    Updates

---
+++ precache-manifest.7dec712f98b2ac0e0dbc5b58290e3431.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6735055d4e58bddfe3bfdca0bffe6cc",
+    "url": "/react-demo-search/index.html"
+  },
+  {
+    "revision": "ffa94fa46ef825496d86",
+    "url": "/react-demo-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "73dfac4a0f2d1a800316",
+    "url": "/react-demo-search/static/css/main.2e6da4ef.chunk.css"
+  },
+  {
+    "revision": "ffa94fa46ef825496d86",
+    "url": "/react-demo-search/static/js/2.307e5030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-search/static/js/2.307e5030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73dfac4a0f2d1a800316",
+    "url": "/react-demo-search/static/js/main.31992b27.chunk.js"
+  },
+  {
+    "revision": "2dbd8656d508ed702b48",
+    "url": "/react-demo-search/static/js/runtime-main.9f90acb9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-demo-search/static/media/SEGA.12a3db8b.woff"
+  }
+]);
